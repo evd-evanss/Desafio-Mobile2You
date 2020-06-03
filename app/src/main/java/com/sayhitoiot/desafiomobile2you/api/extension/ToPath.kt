90e1475f708c5abceb5ba9935c4c2d7211af0a15
evd-evanss <revandro77@yahoo.com.br>
2020-06-02 23:16:51 -0300

    creation of extension to mount url of images

--- app/src/main/java/com/sayhitoiot/desafiomobile2you/api/extension/ToPath.kt
+++ app/src/main/java/com/sayhitoiot/desafiomobile2you/api/extension/ToPath.kt
@@ -1,6 +1,6 @@
-package com.sayhitoiot.desafiomobile2you.api
+package com.sayhitoiot.desafiomobile2you.api.extension
 
-fun String.ToPath() : String {
+fun String.createImagePath() : String {
 
     return "https://image.tmdb.org/t/p/w500/${this}"
 
